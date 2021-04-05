c6b891fd4e555b7473ac8cebaf67ba8a5853c954
choy-ys <project.log.062@gmail.com>
2021-04-05 15:33:07 +0900

    [2021-04-05]용석:JPA와 Spring Data JPA를 이용한 Member CRUD Repository 생성 및 구현
     - JPA를 이용한 Member CRUD Repository 작성
       - Member Entity의 CRUD 구현
       - JPA로 작성된 Member CRUD Repository TC 작성
     - Spring Data JPA를 이용한 Member Repository 생성
       - Spring Data JPA로 작성된 Member CRUD Repository TC 작성
    
     - TC 구동 환경 설정
       - BaseTest : 각 TC에서 공통으로 선언되는 Annotation 설정
       - MemberGenerator : 회원 TC 수행에 필요한 Member Entity 생성

--- src/test/java/io/example/springdatajpa/SpringDataJpaApplicationTests.java
+++ src/test/java/io/example/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,11 +1,13 @@
 package io.example.springdatajpa;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
 @Profile("test")
+@Disabled
 class SpringDataJpaApplicationTests {
 
     @Test
